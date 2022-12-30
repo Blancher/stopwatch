99877c6bc799ac61f116103fbb66d66bcba6f8d8
Blancher <108644584+Blancher@users.noreply.github.com>
2022-12-29 23:00:33 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const resetBtn = document.querySelector('#resetBtn');
 
 // Times
 
+let ms = 0;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
@@ -14,7 +15,11 @@ let displayTimer = document.querySelector('#timer');
 // Stopwatch
 
 function stopWatch() {
-    seconds++;
+    ms++;
+    if (ms / 100 === 1) {
+        ms = 0;
+        seconds++;
+    }
     if (seconds / 60 === 1) {
         seconds = 0;
         minutes++;
@@ -24,24 +29,24 @@ function stopWatch() {
         hours++;
     }
     function addZeroes(time) {
-        const wordTime = time.toString();
-        if ((wordTime + '0').length > 2) {
-            return wordTime;
+        if ((`${time}0`).length > 2) {
+            return time;
         } else {
-            return '0' + wordTime;
+            return `0${time}`;
         }
     }
+    ms = addZeroes(ms);
     seconds = addZeroes(seconds);
     minutes = addZeroes(minutes);
     hours = addZeroes(hours);
-    displayTimer.textContent = hours + ":" + minutes + ":" + seconds;
+    displayTimer.textContent = `${hours}:${minutes}:${seconds}:${ms}`;
 }
 
 // Event listeners
 
 startStopBtn.addEventListener('click', () => {
     if (startStopBtn.textContent === 'Start') {
-        interval = setInterval(stopWatch, 1000);
+        interval = setInterval(stopWatch, 10);
         startStopBtn.textContent = 'Pause';
     } else if (startStopBtn.textContent === 'Pause') {
         clearInterval(interval);
@@ -50,9 +55,10 @@ startStopBtn.addEventListener('click', () => {
 });
 resetBtn.addEventListener('click', () => {
     clearInterval(interval);
+    ms = 0;
     seconds = 0;
     minutes = 0;
     hours = 0;
-    displayTimer.textContent = '00:00:00';
+    displayTimer.textContent = '00:00:00:00';
     startStopBtn.textContent = 'Start';
 });
